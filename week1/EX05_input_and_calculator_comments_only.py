b6bf0e602e58fa65413b2b46d81e75617a668cf0
Devansh <DEVMAN89658@nbss.nbed.nb.ca>
2026-02-07 20:41:54 -0400

    Update EX05_input_and_calculator_comments_only.py

--- week1/EX05_input_and_calculator_comments_only.py
+++ week1/EX05_input_and_calculator_comments_only.py
@@ -1,15 +1,3 @@
-# EX05 - Input + Calculator (COMMENTS ONLY)
-#
-# Goal:
-#   Ask the user for TWO numbers, then print:
-#     Sum: <a+b>
-#     Product: <a*b>
-#
-# Rules:
-#   - Use input() twice.
-#   - Convert both inputs into integers using int().
-#
-# Hint:
-#   a = int(input("Enter a number: "))
-#   b = int(input("Enter another number: "))
-#   print("Sum:", a + b)
+number1=int(input("Enter a number:"))
+number2=int(input("Enter an other number:"))
+print(number1+number2)
