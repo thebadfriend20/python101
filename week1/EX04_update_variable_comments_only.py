78c1a2d845e5c2974470e748fe1f696f148dea13
Devansh <DEVMAN89658@nbss.nbed.nb.ca>
2026-02-07 20:30:33 -0400

    Update EX04_update_variable_comments_only.py

--- week1/EX04_update_variable_comments_only.py
+++ week1/EX04_update_variable_comments_only.py
@@ -1,16 +1,4 @@
-# EX04 - Updating a Variable (COMMENTS ONLY)
-#
-# Goal:
-#   Start with coins = 20
-#   Then:
-#     - Add 10 coins (found treasure)
-#     - Subtract 4 coins (bought a potion)
-#
-# Print coins after each change, like:
-#   Start: 20
-#   After treasure: 30
-#   After potion: 26
-#
-# Rules:
-#   - Use the SAME variable name every time.
-#   - Use coins = coins + 10 and coins = coins - 4 (no shortcuts yet).
+coin=20
+print("start",coin)
+print("After treasure",coin+10)
+print("After Potion",coin-4)
