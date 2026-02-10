31b8f4874d50554e1f5a1d344f7069d23d9efe72
Devansh <DEVMAN89658@nbss.nbed.nb.ca>
2026-02-09 20:16:57 -0400

    Update EX03_if_elif_else_comments_only.py

--- week2/EX03_if_elif_else_comments_only.py
+++ week2/EX03_if_elif_else_comments_only.py
@@ -1,17 +1,10 @@
-# Filename: EX03_if_elif_else_comments_only.py
-# EX03 - if / elif / else (COMMENTS ONLY)
-#
-# Goal:
-#   Ask the user for a temperature in Celsius (int).
-#   Print:
-#     temp >= 25  -> "Hot"
-#     temp >= 15  -> "Warm"
-#     temp >= 5   -> "Cool"
-#     else        -> "Cold"
-#
-# Rules:
-#   - Convert input to int
-#   - Use if / elif / else
-#
-# Hint:
-#   temp = int(input("Temp: ").strip())
+temp = int(input("Temp: ").strip())
+if temp >= 25:
+    print("Hot")
+elif temp >= 15:
+    print("Warm")
+elif temp >= 5:
+    print("Cool")
+else:
+ print("Cold")
+  #by Devansh
